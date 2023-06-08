d4ed28c6b72445b60d48f93d55bebc9b33ec35eb
amccairel <mccairelaw@gmail.com>
2023-06-08 13:36:15 -0600

    styling footer and adding github/linkedin links

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,13 +5,13 @@ import '../styles/global.css'
 const Footer = () => {
     return(
         <footer>
-                <span>Copyright 2023 Alex McCairel</span>
+                <span>&copy; Alex McCairel 2023</span>
                 <div className="social-media-accounts">
                     <a className="social-link" href='https://github.com/amccairel'>
-                        <StaticImage src='../images/GitHub_Logo_White.png' alt='Github Logo'/>
+                        <StaticImage className="github-logo" src='../images/GitHub_Logo_White.png' alt='Github Logo'/>
                     </a>
                     <a className="social-link" href="https://www.linkedin.com/in/alex-mccairel-b0b2b463/">
-                        <StaticImage src="../images/LI-Logo.png" alt="LinkedIn Logo"/>
+                        <StaticImage className="linkedin-logo" src="../images/LI-Logo.png" alt="LinkedIn Logo"/>
                     </a>
                 </div>
         </footer>
